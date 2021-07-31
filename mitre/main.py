94c143d04c360c03653239c1bb03bbeadb490762
sakshee-sirpi <sakshee1120@gmail.com>
2021-08-01 00:00:56 +0530

    requirements added, main.py changed to python3

--- mitre/main.py
+++ mitre/main.py
@@ -1,6 +1,6 @@
 import os
 
 os.system('scrapy crawl metrics')
-os.system('python create_index.py')
-os.system('python json_to_es.py')
+os.system('python3 create_index.py')
+os.system('python3 json_to_es.py')
 os.remove('MitreData.json')
